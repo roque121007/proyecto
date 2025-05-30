6569d2caf37b85ffef69d9e66102137b27f7b1ae
roque121007 <jesusalexanderroquetrejo90@gmail.com>
2025-05-29 19:08:04 -0600

    formato de cadenas y corrección de variables en MoviesProvider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -4,19 +4,21 @@ import 'package:proyecto_peliculas/models/model.dart';
 import 'package:proyecto_peliculas/models/serach_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
-  final String _apiKey = "f75c8b6efbddf53da4f286e1a81c3e65";
-  final String _baseUrl = "api.themoviedb.org";
-  final String _languaje = "es-ES";
+  final String _apiKey = 'f75c8b6efbddf53da4f286e1a81c3e65';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _languaje = 'es-ES';
 
   List<Movie> onDisplayMovies = [];
   List<Movie> popularMovies = [];
   Map<int, List<Cast>> moviesCast = {};
-  int _popularPage = 0;
+
+  int _popularpage = 0;
 
   MoviesProvider() {
     getOnDisplayMovies();
     getPopularMovies();
   }
+
   Future<String> _getJsonData(String segmento, [int page = 1]) async {
     var url = Uri.https(_baseUrl, segmento, {
       'api_key': _apiKey,
@@ -29,35 +31,40 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getOnDisplayMovies() async {
-    final jsonData = await _getJsonData("3/movie/now_playing");
+    final jsonData = await _getJsonData('3/movie/now_playing');
     final nowPlayingResponse = NowPlayingResponse.fromJson(jsonData);
+
     onDisplayMovies = nowPlayingResponse.results;
-    notifyListeners();
+    //COmo se hace una modificacion y necesitamos redibujar widgets se utiliza notifyListeners
+    notifyListeners(); //A todos los widgets que estan escuchando
   }
 
   getPopularMovies() async {
-    _popularPage++;
-    final jsonData = await _getJsonData("3/movie/popular", _popularPage);
-
+    _popularpage++;
+    final jsonData = await _getJsonData('3/movie/popular', _popularpage);
     final popularResponse = PopularResponse.fromJson(jsonData);
+
     popularMovies = [...popularMovies, ...popularResponse.results];
     notifyListeners();
   }
 
   Future<List<Cast>> getMovieCast(int movieId) async {
-    if (moviesCast.containsKey(movieId)) return moviesCast[movieId]!;
-    final jsonData = await _getJsonData("3/movie/$movieId/credits");
+    if (moviesCast.containsKey(movieId)) {
+      return moviesCast[movieId]!;
+    }
 
+    final jsonData = await _getJsonData('3/movie/$movieId/credits');
     final creditResponse = CreditResponse.fromJson(jsonData);
+
     moviesCast[movieId] = creditResponse.cast;
     return creditResponse.cast;
   }
 
   Future<List<Movie>> searchMovies(String query) async {
-    final url = Uri.https(_baseUrl, "3/search/movie", {
+    final url = Uri.https(_baseUrl, '3/search/movie', {
       'api_key': _apiKey,
       'language': _languaje,
-      'page': query,
+      'query': query,
     });
 
     final response = await http.get(url);
